10b13d66391ff0491ff23bc4ebdd26eaabafa967
Michael Zinoviev <mishazinoviev91@gmail.com>
2021-09-17 18:17:39 +0300

    .

--- middlewares/login-filter.js
+++ middlewares/login-filter.js
@@ -5,7 +5,7 @@ let { secret } = config;
 
 function authenticateJwtRequestToken() {
   return expressJwt({ secret, algorithms: ['HS256'] }).unless({
-    path: ['/users/login', '/shop', '/users/register','/users/register2'],
+    path: ['/users/login', '/users', '/shop', '/users/register','/users/register2'],
   });
 }
 
